e92acd87816c5c25a94c520b38e0788fdaf947e1
vinayak pagar <vinayak@vinayaks-MacBook-Pro.local>
2024-09-20 12:34:38 +0530

    fix:on production accessToken generate

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ UsersSchema.methods.generateAccessToken = async function () {
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: `${process.env.ACCESS_TOKEN_EXPIRY}`,
+      expiresIn: `1d`,
     }
   );
 };
@@ -62,7 +62,7 @@ UsersSchema.methods.generateRefreshToken = async function () {
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-          expiresIn: `${process.env.REFRESH_TOKEN_EXPIRY}`,
+          expiresIn: `7d`,
         }
       );
 };
